915e368589ae3eb0eb1be07741ceb9e7cda71913
jdcharbonneau <john@trainsarecool.com>
2016-02-14 00:32:38 +0700

    2nd Commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var addx = function(x){ return function(y){return x+y;}} 
 var add2 = addx(2);
-console.log(add2(99))
 var a = [1,2,3,5,7,9];
 var b = (a.map(x=> add2(x))).map(x=>x+3);
 console.log(b)
+console.log(b+b)
